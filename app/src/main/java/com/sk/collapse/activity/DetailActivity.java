91e65b007c007be5aef2df47ce94bdd5a531f1ba
shikunxh@126.com <shikunxh@126.com>
2016-09-03 12:43:01 +0800

    modify the metre of collapsingToolBarLayout control

--- app/src/main/java/com/sk/collapse/activity/DetailActivity.java
+++ app/src/main/java/com/sk/collapse/activity/DetailActivity.java
@@ -2,8 +2,10 @@ package com.sk.collapse.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 /**
  * Created by sk on 16-9-2.
@@ -11,6 +13,7 @@ import android.support.v7.widget.Toolbar;
 public class DetailActivity extends BaseAppCompatActivity {
 
     private Toolbar mToolBar;
+    private CollapsingToolbarLayout mCollapse;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -27,6 +30,18 @@ public class DetailActivity extends BaseAppCompatActivity {
             ab.setDisplayShowTitleEnabled(true);
             ab.setDisplayUseLogoEnabled(true);
         }
+
+        mCollapse = (CollapsingToolbarLayout) findViewById(R.id.id_collapse);
+//        mCollapse.setExpandedTitleColor(Color.WHITE);
+//        mCollapse.setCollapsedTitleTextColor(Color.WHITE);
+//        mCollapse.setExpandedTitleMarginBottom(30);
+
+        mToolBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
 
     }
 }
