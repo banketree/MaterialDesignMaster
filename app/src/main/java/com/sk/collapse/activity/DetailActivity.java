1d84ee266c6d27f72f822efabcd31a8c16c73716
shikunxh@126.com <shikunxh@126.com>
2016-09-03 16:26:39 +0800

    add snackbar control using

--- app/src/main/java/com/sk/collapse/activity/DetailActivity.java
+++ app/src/main/java/com/sk/collapse/activity/DetailActivity.java
@@ -3,6 +3,8 @@ package com.sk.collapse.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -14,6 +16,7 @@ public class DetailActivity extends BaseAppCompatActivity {
 
     private Toolbar mToolBar;
     private CollapsingToolbarLayout mCollapse;
+    private FloatingActionButton mFloatBtn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -43,5 +46,23 @@ public class DetailActivity extends BaseAppCompatActivity {
             }
         });
 
+
+        //floating button
+        mFloatBtn = (FloatingActionButton) findViewById(R.id.id_floatbtn);
+
+        mFloatBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Snackbar sb = Snackbar.make(mFloatBtn, R.string.detail, Snackbar.LENGTH_LONG);
+
+                        sb.setAction(R.string.app_name, new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                sb.dismiss();
+                            }
+                        }).show();
+
+            }
+        });
     }
 }
